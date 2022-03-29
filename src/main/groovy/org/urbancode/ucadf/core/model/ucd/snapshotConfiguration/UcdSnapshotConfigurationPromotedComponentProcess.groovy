845a149ee8a96a96e1543a5f00c97f45f49ccf70
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-29 11:14:31 +0800

    Delete src directory

--- src/main/groovy/org/urbancode/ucadf/core/model/ucd/snapshotConfiguration/UcdSnapshotConfigurationPromotedComponentProcess.groovy
+++
@@ -1,8 +0,0 @@
-package org.urbancode.ucadf.core.model.ucd.snapshotConfiguration
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-
-//@JsonIgnoreProperties(ignoreUnknown = true)
-class UcdSnapshotConfigurationPromotedComponentProcess extends UcdSnapshotConfigurationProcess implements UcdSnapshotConfigurationTypeByClassName {
-	public final static String CLASS_NAME = "PromotedComponentProcess"
-}
--- src/main/groovy/org/urbancode/ucadf/core/model/ucd/snapshotConfiguration/UcdSnapshotConfigurationVersionProperties.groovy
+++
@@ -1,9 +0,0 @@
-package org.urbancode.ucadf.core.model.ucd.snapshotConfiguration
-
-import org.urbancode.ucadf.core.model.ucd.general.UcdObject
-
-class UcdSnapshotConfigurationVersionProperties extends UcdSnapshotConfigurationEntity {
-	public final static String TYPE_NAME = "Properties for Component"
-	
-	List<UcdSnapshotConfigurationPropSheet> children
-}
